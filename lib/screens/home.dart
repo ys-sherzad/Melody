f0859a6fe9b3721200db7a35783cb9eb9feff95a
YasirSherzad <ys.sherzad@gmail.com>
2021-11-09 21:02:24 +0430

    project scaffold

---
+++ lib/screens/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class Home extends HookWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        children: [],
+      )),
+    );
+  }
+}
