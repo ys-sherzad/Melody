4a53fe25f73b6bd4812a3edde3a59943f9aac9f9
YasirSherzad <ys.sherzad@gmail.com>
2021-11-10 19:07:25 +0430

    add background images

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:melody/screens/home.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setSystemUIOverlayStyle(
+  //   const SystemUiOverlayStyle(
+  //     statusBarColor: Colors.transparent, // transparent status bar
+  //   ),
+  // );
   runApp(const MyApp());
 }
 
